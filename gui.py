27d900d5f12ee4b7f334980dbfa9472eae4e3576
dipanshgarg21 <95221843+dipanshgarg21@users.noreply.github.com>
2022-10-18 00:11:18 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter.font
 import RPi.GPIO as GPIO
 from gpiozero import LED
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 led1 = LED(14)
 led2 = LED(15)
@@ -10,47 +10,43 @@ led3 = LED(18)
 
 master = Tk()
 master.title("MENU")
-myFont = tkinter.font.Font(family = 'Helvetica' , size = 12, weight = "bold")
+myFont = tkinter.font.Font(family = 'Arial' , size = 12, weight = "bold")
+
+var = StringVar()
+var.set("LED")
 
 def ledBlink1():
     if led1.is_lit:
         led1.off()
-        ledButton1["text"] = "LED: SWITCH ON"
     else:
         led1.on()
-        ledButton1["text"] = "LED: SWITCH OFF"
         
 def ledBlink2():
     if led2.is_lit:
         led2.off()
-        ledButton2["text"] = "LED: SWITCH ON"
     else:
         led2.on()
-        ledButton2["text"] = "LED: SWITCH OFF"
 
 def ledBlink3():        
     if led3.is_lit:
         led3.off()
-        ledButton3["text"] = "LED: SWITCH ON"
     else:
         led3.on()
-        ledButton3["text"] = "LED: SWITCH OFF"
-
+        
 def close():
     GPIO.cleanup()
     master.destroy()
 
-ledButton1 = Button(master, text = 'LED: SWITCH ON', font = myFont, command = ledBlink1, bg = 'blue', height = 2, width = 30)
-ledButton1.grid(row=0, column=1)
+Label(master, text = "Choose an LED", font = myFont, padx = 14).pack()
+ledButton1 = Radiobutton(master, text = "RED", font = myFont, command = ledBlink1, variable = var, var = "Red").pack(side = LEFT)
+ledButton2 = Radiobutton(master, text = "GREEN",font = myFont, command = ledBlink2, variable = var, var = "GREEN").pack(side = LEFT)
+ledButton3 = Radiobutton(master, text = "BLUE", font = myFont, command = ledBlink3, variable = var, var = "BLUE").pack(side = LEFT)
 
-ledButton2 = Button(master, text = 'LED: SWITCH ON', font = myFont, command = ledBlink2, bg = 'green', height = 2, width = 30)
-ledButton2.grid(row=1, column=1)
+exitButton = Button(master, text = 'QUIT', font = myFont, command = close, bg = 'purple', height = 1, width = 13).pack(anchor = "w")
 
-ledButton3 = Button(master, text = 'LED: SWITCH ON', font = myFont, command = ledBlink3, bg = 'red', height = 2, width = 30)
-ledButton3.grid(row=2, column=1)
-
-exitButton = Button(master, text = 'QUIT', font = myFont, command = close, bg = 'purple', height = 1, width = 13)
-exitButton.grid(row=3, column=1)
-
-master.protocol("WM_DELETE_WINDOW", close)
+master.protocol("WM_DELETE_MASTER", close)
 master.mainloop()
+
+
+
+
